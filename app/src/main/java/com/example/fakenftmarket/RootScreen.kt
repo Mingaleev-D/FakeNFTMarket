53a4f2f8a15e4adde0429ccfacea0c6a909b38f6
admin <mingaleev.d92@mail.ru>
2023-05-21 01:00:07 +0300

    панель управления (пока что без функционала)

--- app/src/main/java/com/example/fakenftmarket/RootScreen.kt
+++ app/src/main/java/com/example/fakenftmarket/RootScreen.kt
@@ -1,7 +1,18 @@
 package com.example.fakenftmarket
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.BottomNavigation
+import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
 import com.example.fakenftmarket.ui.home.HomeScreen
 
 /**
@@ -11,7 +22,56 @@ import com.example.fakenftmarket.ui.home.HomeScreen
 
 @Composable
 fun RootScreen() {
-   Scaffold {
+   Scaffold(bottomBar = {
+      BottomBar()
+   }) {
       HomeScreen()
+   }
+}
+
+sealed class NavigationItem(
+    var route: String,
+    var icon: ImageVector,
+    var title: String
+) {
+   object Login : NavigationItem("login", Icons.Filled.Home, "Home")
+   object Home : NavigationItem("home", Icons.Filled.Home, "Home")
+   object Stats : NavigationItem("stats", Icons.Filled.Analytics, "Stats")
+   object Add : NavigationItem("add", Icons.Filled.Add, "Add")
+   object Search : NavigationItem("search", Icons.Filled.Search, "Search")
+   object Profile : NavigationItem("profile", Icons.Filled.Person, "Profile")
+}
+
+@Composable
+fun BottomBar() {
+   val items = listOf(
+       NavigationItem.Home,
+       NavigationItem.Stats,
+       NavigationItem.Add,
+       NavigationItem.Search,
+       NavigationItem.Profile
+   )
+   BottomNavigation(
+       backgroundColor = Color(33, 17, 52),
+       contentColor = Color.White
+   ) {
+      items.forEach { item ->
+         BottomNavigationItem(
+             selected = false, onClick = { /*TODO*/ },
+             icon = {
+                Image(
+                    imageVector = item.icon,
+                    contentDescription = item.title,
+                    modifier = Modifier.size(30.dp),
+                    colorFilter = ColorFilter.tint(Color.White)
+                )
+
+             },
+             selectedContentColor = Color.White,
+             unselectedContentColor = Color.White.copy(0.4f),
+             alwaysShowLabel = false
+
+         )
+      }
    }
 }
